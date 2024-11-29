43891c68f83247885f0e34dc5d24343702c81d75
Rikard Karlsson <karlsson0214@gmail.com>
2024-11-29 12:51:06 +0100

    jump start

--- MyWorld.cs
+++ MyWorld.cs
@@ -5,14 +5,26 @@ using System.Text;
 using System.Threading.Tasks;
 using EasyMonoGame;
 
-namespace EasyStart
+namespace EasyDemoActJump
 {
     internal class MyWorld : World
     {
-        public MyWorld() : base(600, 800)
+        public MyWorld() : base(800, 600)
         {
             // Tile background with the file "bluerock" in the Content folder.
             BackgroundTileName = "bluerock";
-        }  
+
+            Add(new Player(), "kangaroo", 400, 100);
+
+            for (int x = 0; x < 825; x += 25)
+            {
+                Add(new Ground(), "brick", x, 550);
+            }
+            for (int x = 0; x < 325; x += 25)
+            {
+                Add(new Ground(), "brick", x, 450);
+            }
+
+        }
     }
 }
---
+++ Player.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EasyMonoGame;
+using Microsoft.Xna.Framework.Input;
+
+namespace EasyDemoActJump
+{
+    internal class Player : Actor
+    {
+        private float gravity = 0.1f;
+        private float ySpeed = 0;
+        public override void Act()
+        {
+            if (IsTouching(typeof(Ground)))
+            {
+                ySpeed = 0;
+            }
+            else
+            {
+                ySpeed += gravity;
+                Y += ySpeed;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Space) && OnGround())
+            {
+                ySpeed = -5;
+                Y += ySpeed;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                X -= 3;
+            }
+            else if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                X += 3;
+            }
+
+
+
+        }
+        private bool OnGround()
+        {
+            if (IsTouching(typeof(Ground)))
+            {
+                Ground ground = (Ground)GetOneIntersectingActor(typeof(Ground));
+                // is above?
+                if ( ground.Y - 10 - Y + 25> 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
