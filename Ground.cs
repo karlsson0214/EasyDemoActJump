7213ff9e8f05123b35f0d31ebfbad3dddfdfc000
Rikard Karlsson <karlsson0214@gmail.com>
2024-12-02 21:00:53 +0100

    Same direction as in player.

--- Ground.cs
+++ Ground.cs
@@ -10,35 +10,35 @@ namespace EasyDemoActJump
     internal class Ground : Actor
     {
         private bool isMoveable = false;
+        private float ySpeed = -3;
 
         public bool IsMoveable
         {
             get { return isMoveable; }
             set { isMoveable = value; }
         }
-        private float ySpeed = 3;
+        
         public override void Act()
         {
             if (isMoveable)
             {
                 Move();
             }
-            
         }
         private void Move()
         {
             if (Y >= 450)
             {
                 // move up  
-                ySpeed = 3;
+                ySpeed = -3;
 
             }
             if (Y <= 150)
             {
                 // move down
-                ySpeed = -3;
+                ySpeed = 3;
             }
-            Y -= ySpeed;
+            Y += ySpeed;
         }
     }
 }
