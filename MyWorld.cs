2bfaf5953943d25ed91a62563b39f9af40738834
Rikard Karlsson <karlsson0214@gmail.com>
2024-12-02 20:57:56 +0100

    More ground and a moving platfrom.

--- MyWorld.cs
+++ MyWorld.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using EasyMonoGame;
 
-namespace EasyDemoActJump
+namespace EasyDemoActJump 
 {
     internal class MyWorld : World
     {
@@ -16,14 +16,24 @@ namespace EasyDemoActJump
 
             Add(new Player(), "kangaroo", 400, 100);
 
+            // ground - bottom
             for (int x = 0; x < 825; x += 25)
             {
                 Add(new Ground(), "brick", x, 550);
             }
-            for (int x = 0; x < 325; x += 25)
+            // platforms to left
+            for (int y = 150; y <= 450; y += 100)
             {
-                Add(new Ground(), "brick", x, 450);
+                for (int x = 0; x < 325; x += 25)
+                {
+                    Add(new Ground(), "brick", x, y);
+                }
             }
+            // elevator
+            Ground elevator = new Ground();
+            elevator.IsMoveable = true;
+            Add(elevator, "brick", 350, 450);
+
 
         }
     }
