e98c2329164b538cb97c09138dc53d89ce0a0725
Rikard Karlsson <karlsson0214@gmail.com>
2024-12-02 20:56:20 +0100

    jump works

--- Player.cs
+++ Player.cs
@@ -12,46 +12,89 @@ namespace EasyDemoActJump
     {
         private float gravity = 0.1f;
         private float ySpeed = 0;
+        private float xSpeed = 0;
         public override void Act()
         {
-            if (IsTouching(typeof(Ground)))
+            if (InAir())
             {
-                ySpeed = 0;
+                MoveByGravity();
             }
             else
             {
-                ySpeed += gravity;
-                Y += ySpeed;
+                // on ground
+                CheckMove();
             }
-            if (Keyboard.GetState().IsKeyDown(Keys.Space) && OnGround())
-            {
-                ySpeed = -5;
-                Y += ySpeed;
-            }
-            if (Keyboard.GetState().IsKeyDown(Keys.Left))
-            {
-                X -= 3;
-            }
-            else if (Keyboard.GetState().IsKeyDown(Keys.Right))
-            {
-                X += 3;
-            }
-
-
-
         }
         private bool OnGround()
         {
             if (IsTouching(typeof(Ground)))
             {
                 Ground ground = (Ground)GetOneIntersectingActor(typeof(Ground));
-                // is above?
-                if ( ground.Y - 10 - Y + 25> 0)
+                // on ground - max 10 pixel into ground from above
+                if ( Y + Height / 2 < ground.Y - ground.Height / 2 + 10 )
                 {
                     return true;
                 }
             }
             return false;
+        }
+        private bool InAir()
+        {
+            return !OnGround();
+        }
+        private void MoveByGravity()
+        {
+            ySpeed += gravity;
+            Y += ySpeed;
+            X += xSpeed;
+            if (IsTouching(typeof(Ground)))
+            {
+                Actor ground = GetOneIntersectingActor(typeof(Ground));
+                if (Y < ground.Y)
+                {
+                    // from above - stop at ground
+                    float deltaY = Y + Height / 2 - ground.Y + ground.Height / 2;
+                    // 1 pixel inside the ground
+                    Y -= deltaY + 1;
+                    ySpeed = 0;
+                }
+                else
+                {
+                    // from below - stop at roof
+                    float deltaY = ground.Y + ground.Height / 2 - Y + Height / 2;
+                    // keep 1 pixel below roof
+                    Y += deltaY +10;
+                    ySpeed = 0;
+                }
+            }
+        }
+        private void CheckMove()
+        {
+            if (OnGround())
+            {
+                if (Keyboard.GetState().IsKeyDown(Keys.Left))
+                {
+                    xSpeed = -3;
+                    X += xSpeed;
+                    IsFlippedHorizontally = true;
+                }
+                else if (Keyboard.GetState().IsKeyDown(Keys.Right))
+                {
+                    xSpeed = 3;
+                    X += xSpeed;
+                    IsFlippedHorizontally = false;
+                }
+                else
+                {
+                    xSpeed = 0;
+                }
+                if (Keyboard.GetState().IsKeyDown(Keys.Space) || 
+                    Keyboard.GetState().IsKeyDown(Keys.Up))
+                {
+                    ySpeed = -5;
+                    Y += ySpeed;
+                }
+            }
         }
     }
 }
