f56013573db2d938308a626bcd1ab754a728e2d8
Rikard Karlsson <karlsson0214@gmail.com>
2024-12-02 20:57:34 +0100

    May be a moving platform.

--- Ground.cs
+++ Ground.cs
@@ -9,5 +9,36 @@ namespace EasyDemoActJump
 {
     internal class Ground : Actor
     {
+        private bool isMoveable = false;
+
+        public bool IsMoveable
+        {
+            get { return isMoveable; }
+            set { isMoveable = value; }
+        }
+        private float ySpeed = 3;
+        public override void Act()
+        {
+            if (isMoveable)
+            {
+                Move();
+            }
+            
+        }
+        private void Move()
+        {
+            if (Y >= 450)
+            {
+                // move up  
+                ySpeed = 3;
+
+            }
+            if (Y <= 150)
+            {
+                // move down
+                ySpeed = -3;
+            }
+            Y -= ySpeed;
+        }
     }
 }
